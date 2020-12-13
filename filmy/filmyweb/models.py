c3a8167a7d4d7a5638a871938affe5c346352fb3
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-13 21:59:44 +0100

    Create template

--- filmy/filmyweb/models.py
+++ filmy/filmyweb/models.py
@@ -8,7 +8,8 @@ class Film(models.Model):
     premiera = models.DateField(null=True, blank=True)
     imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,null=True, blank = 
     False)
-    plakat = models.ImageField(upload_to='plakaty', null=True, blank = False)
+    plakat = models.ImageField(upload_to='plakaty', null=True, blank =
+    True)
 
     def __str__(self):
         return self.tytul
