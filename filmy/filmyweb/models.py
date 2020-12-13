57710381cd31453e0e302358601667188f4dc9a1
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-13 21:00:24 +0100

    Create model class

--- filmy/filmyweb/models.py
+++ filmy/filmyweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    tytul = models.CharField(max_length=100)
+    rok = models.PositiveSmallIntegerField(default=2000)
+
+
+    def __str__(self):
+        return self.tytul
