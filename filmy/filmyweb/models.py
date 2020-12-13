a6983be4a46d2a1fd0b803ad834dfb7c89c10995
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-13 21:20:26 +0100

    Update model class

--- filmy/filmyweb/models.py
+++ filmy/filmyweb/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Film(models.Model):
     tytul = models.CharField(max_length=100)
     rok = models.PositiveSmallIntegerField(default=2000)
-
+    opis = models.TextField(default='')
+    premiera = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,null=True, blank = 
+    False)
+    plakat = models.ImageField(upload_to='plakaty', null=True, blank = False)
 
     def __str__(self):
         return self.tytul
