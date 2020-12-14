a1b1507724b4f2edbbd5693ca29004c57eb78f55
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-14 18:37:10 +0100

    Create 'login.html' file

--- filmy/filmyweb/urls.py
+++ filmy/filmyweb/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.wszystkie_filmy),
-    path('nowy/', views.nowy_film),
-    path('edytuj/<int:id>/', views.edytuj_film),
-    path('usun/<int:id>/', views.usun_film),
+    path('', views.wszystkie_filmy, name = 'wszystkie_filmy'),
+    path('nowy/', views.nowy_film, name = 'nowy_film'),
+    path('edytuj/<int:id>/', views.edytuj_film, name='edytuj_film'),
+    path('usun/<int:id>/', views.usun_film, name = 'usun_film'),
 ]
