d98d560fd65b6a2c0db77ce67a85c3719512307d
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-14 19:48:59 +0100

    Update login/logout files

--- filmy/filmy/urls.py
+++ filmy/filmy/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('filmyweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
