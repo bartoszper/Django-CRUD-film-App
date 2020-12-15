64b1093fc52f758c13a643b15351b9b59958a36a
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-15 19:42:18 +0100

    Update for Heroku deployment

--- filmy/filmy/settings.py
+++ filmy/filmy/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from decouple import config
+from dj_database_url import parse
 
 from pathlib import Path
 
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5h+*irh9)=-*f=1amwyt+!$%ez)j%^#+@$rvxgw9_=ul1wm#df'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
