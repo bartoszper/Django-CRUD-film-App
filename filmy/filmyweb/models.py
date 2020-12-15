7030c0ce76cd030519ffd4035e87936f7ae19dd4
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-15 12:54:34 +0100

    Create new model 'DodatkoweInfo' in models.py

--- filmy/filmyweb/models.py
+++ filmy/filmyweb/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class DodatkoweInfo(models.Model):
+    GATUNEK = {
+        (0, 'Inny'),
+        (1, 'Horror'),
+        (2, 'Komedia'),
+        (3, 'Sci-Fy'),
+        (4, 'Dramat'),
+    }
+
+    czas_trwania = models.PositiveSmallIntegerField(default=0)
+    gatunek = models.PositiveSmallIntegerField(default = 0, choices= GATUNEK )
+
+
 class Film(models.Model):
     tytul = models.CharField(max_length=100)
     rok = models.PositiveSmallIntegerField(default=2000, blank=True)
@@ -9,6 +21,7 @@ class Film(models.Model):
     imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,null=True, blank = True)
     plakat = models.ImageField(upload_to='plakaty', null=True, blank =
     True)
+    dodatkoweinfo = models.OneToOneField(DodatkoweInfo, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.tytul
